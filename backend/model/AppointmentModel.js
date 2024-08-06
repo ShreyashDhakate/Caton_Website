9a3f2e416d4cb77e87b50be3f13ac985bd373780
Shreyash Dhakate <161032146+ShreyashDhakate@users.noreply.github.com>
2024-08-06 23:23:17 +0530

    Delete backend directory

--- backend/model/AppointmentModel.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const {AppointmentSchema} = require('../schemas/AppointmentSchema'); 
-
-const AppointmentModel = mongoose.model('Appointment', AppointmentSchema);
-
-module.exports = {AppointmentModel};
-
-
